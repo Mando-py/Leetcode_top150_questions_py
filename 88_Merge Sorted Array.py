f8ae73841665e555afa0835d68825ce200475b2c
Mando-py <shanmukha.de@gmail.com>
2024-08-25 18:07:42 -0500

    Add files via upload

---
+++ 88_Merge Sorted Array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def merge(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
+        del nums1[m:]
+        nums1.extend(nums2)
+        nums1.sort()
+
+sul = Solution()
+
+
